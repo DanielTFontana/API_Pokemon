8d725682ba74b201e500c7944b9c9b0051a82d8d
Daniel T Fontana <daniel.dtf@icloud.com>
2024-03-22 01:01:08 -0300

    chart insertion

--- src/Components/OpenPokeDex/Card/index.tsx
+++ src/Components/OpenPokeDex/Card/index.tsx
@@ -19,6 +19,7 @@ import ghost from "../../../assets/ghost.png";
 import dragon from "../../../assets/dragon.png";
 import steel from "../../../assets/steel.png";
 import fairy from "../../../assets/fairy.png";
+import { BarChart } from "@mui/x-charts/BarChart";
 
 const PokemonTypes: any = {
   normal,
@@ -44,21 +45,33 @@ interface CardProps {
   selectedPokemon: string;
 }
 
-export const Card: React.FC<CardProps> = ({selectedPokemon }) => {
+interface PokeData {
+  data: any;
+  stats: [PokemonStat];
+}
+
+interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: "";
+    url?: "";
+  };
+}
+
+export const Card: React.FC<CardProps> = ({ selectedPokemon }) => {
   const [pokemon, setPokemon] = useState<any>([]);
   const [dataOn, setDataOn] = useState<boolean>(false);
   const [transform, setTransform] = useState("default");
   const [pokeType, setPokeType] = useState<string[]>([]);
-
-  interface PokeData {
-    data:any
-  }
+  const [statsOrganized, setStatsOrganized] = useState<any>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data }:PokeData = await API.get(`/${selectedPokemon}`);
+        const { data }: PokeData = await API.get(`/${selectedPokemon}`);
         setPokemon(data);
+        generteStats(data);
         setDataOn(true);
       } catch (error) {
         console.error(error);
@@ -67,9 +80,9 @@ export const Card: React.FC<CardProps> = ({selectedPokemon }) => {
 
     if (selectedPokemon) {
       fetchData();
-  }
+    }
   }, [selectedPokemon]);
-  
+
   const transformPoke = () => {
     transform === "default" ? setTransform("shiny") : setTransform("default");
   };
@@ -78,54 +91,87 @@ export const Card: React.FC<CardProps> = ({selectedPokemon }) => {
     let arrayType: string[] = [];
     pokemons?.types?.map((types: any) => {
       arrayType.push(types.type.name);
-       setPokeType(arrayType);
+      setPokeType(arrayType);
     });
+  };
+
+  const generteStats = (data: PokeData) => {
+    const statsArray: any[] = data.stats.map((stat) => ({
+      base_stat: stat.base_stat,
+      effort: stat.effort,
+    }));
+    setStatsOrganized(statsArray);
+  };
+
+  console.log(statsOrganized);
+
+  // const generateChart = (data: []) => {
+  //   return(
+
+  //   )
+  // }
+
+  const chartSetting = {
+    xAxis: [
+      {
+        label: "Pokemon Stats",
+      },
+    ],
+    width: 500,
+    height: 400,
   };
 
   const renderTypeImages = () => {
     return pokeType.map((type, index) => (
-
-        <img
-          className="pokeTypes"
-          key={index}
-          src={PokemonTypes[type]}
-          alt={type}
-        />
-
+      <img
+        className="pokeTypes"
+        key={index}
+        src={PokemonTypes[type]}
+        alt={type}
+      />
     ));
   };
 
   useEffect(() => {
     if (pokemon) {
       mapingPokeTypes(pokemon);
+      // generateChart(statsOrganized)
     }
   }, [pokemon]);
 
-
-
   return (
- 
-      <div className="biggerCard">
-        <p className="pokeName">{pokemon.name}</p>
-        <div>
-          {renderTypeImages()}
-          {pokeType.length === 1 && <img src={PokemonTypes[""]} alt="" />}
-        </div>
-        <div onClick={transformPoke} className="pokeNball">
-          {dataOn == true && (
-            <img
+    <div className="biggerCard">
+      <p className="pokeName">{pokemon.name}</p>
+      <div>
+        {renderTypeImages()}
+        {pokeType.length === 1 && <img src={PokemonTypes[""]} alt="" />}
+      </div>
+      <div onClick={transformPoke} className="pokeNball">
+        {dataOn == true && (
+          <img
             className="poke"
             src={
-              pokemon?.sprites.versions["generation-v"]["black-white"]
-              .animated[`front_${transform}`]
+              pokemon?.sprites.versions["generation-v"]["black-white"].animated[
+                `front_${transform}`
+              ]
             }
-            />
-            )}
-          <img className="pokeball" src={pokeball} />
-        </div>
+          />
+        )}
+        <img className="pokeball" src={pokeball} />
       </div>
-      
-    
-
+      <div className="cardStats">
+        <BarChart
+          dataset={statsOrganized}
+          series={[{ dataKey: "nome", label: pokemon.name + "status" }]}
+          layout="horizontal"
+          yAxis={[
+            {
+              scaleType: "band",
+              dataKey: "Pokemon",
+            },
+          ]}
+        />
+      </div>
+    </div>
   );
 };
