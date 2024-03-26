e0e199b71637aabbb6c32086f9143071e9abf329
Daniel T Fontana <daniel.dtf@icloud.com>
2024-03-26 00:43:57 -0300

    ferature button and img

--- src/Components/OpenPokeDex/Card/index.tsx
+++ src/Components/OpenPokeDex/Card/index.tsx
@@ -20,6 +20,7 @@ import dragon from "../../../assets/dragon.png";
 import steel from "../../../assets/steel.png";
 import fairy from "../../../assets/fairy.png";
 import { StatsCard } from "./statsCard.tsx/statsCard";
+import choosePoke from '../../../assets/choose_pokemon-removebg-preview.png'
 
 const PokemonTypes: any = {
   normal,
@@ -129,7 +130,7 @@ export const Card: React.FC<CardProps> = ({ selectedPokemon }) => {
       <p className="pokeName">{pokemon.name}</p>
       <div>
         {renderTypeImages()}
-        {pokeType.length === 1 && <img src={PokemonTypes[""]} alt="" />}
+        {pokeType.length === 1 ? <img src={PokemonTypes[""]} alt="" /> :  pokeType.length === 0 && <img style={{filter:' brightness(0) invert(1) saturate(100%)'}} src={choosePoke} alt="choose you poke" /> }
       </div>
       <div onClick={transformPoke} className="pokeNball">
         {dataOn == true && (
--- src/Components/OpenPokeDex/Card/statsCard.tsx/statsCard.tsx
+++ src/Components/OpenPokeDex/Card/statsCard.tsx/statsCard.tsx
@@ -1,36 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
+import arrowDown from "../../../../assets/arrow_down.png";
+import "./statsCard.css";
 
 interface StatsCardProps {
-  stats: any; 
-  pokemonName: string; 
+  stats: any;
+  pokemonName: string;
 }
 
 export const StatsCard: React.FC<StatsCardProps> = ({ stats, pokemonName }) => {
+  const [statsOpen, setStatsOpen] = useState(false);
+
+  const handleButton = () => {
+    setStatsOpen(!statsOpen);
+  };
+    
   const generateChart = () => {
-    if (stats.length === 0) {
-      return null;
-    }
     return (
-      <div className="cardStats">
-        <BarChart
-          dataset={stats}
-          series={[{ dataKey: "base_stat", label: `${pokemonName} status` }]}
-          layout="horizontal"
-          yAxis={[
-            {
-              scaleType: "band",
-              dataKey: "name",
-            },
-          ]}
-          xAxis={[{ min: 0, max: 200 }]}
-          width={350}
-          height={300}
-        />
+      <div className="statsContainer">
+        <button className="btn" onClick={handleButton}>
+          <label className="statsLabel" htmlFor="arrowIcon">
+            Stats
+          </label>{" "}
+          <img
+            className={statsOpen === false ? "arrowIcon" : "arrowIconRevert"}
+            src={arrowDown}
+          />
+        </button>
+  
+        {stats.length !== 0 && (
+          <div className="cardStats">
+            <BarChart
+              dataset={stats}
+              series={[{ dataKey: "base_stat", label: `${pokemonName} status` }]}
+              layout="horizontal"
+              yAxis={[
+                {
+                  scaleType: "band",
+                  dataKey: "name",
+                },
+              ]}
+              xAxis={[{ min: 0, max: 200 }]}
+              width={350}
+              height={300}
+            />
+          </div>
+        )}
       </div>
     );
   };
+  
+  return generateChart();
+  
 
   return generateChart();
 };
-
